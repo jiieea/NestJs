937c945e2644a236f97817a5cc01cff201867a45
Jiéyra De Châtillon <seigneur.noir15@gmail.com>
2025-12-16 07:14:54 +0700

    http method request

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Param, Post, Query } from '@nestjs/common';
 @Controller('/api/users')
-export class UserController {}
+export class UserController {
+  @Post()
+  post(): string {
+    return 'Post';
+  }
+
+  @Get('/auth')
+  get(): string {
+    return 'get method';
+  }
+
+  @Get('/user')
+  sayHello(
+    @Query('first_name') firstName: string,
+    @Query('last_name') lastName: string,
+  ): string {
+    return `hello ${firstName} ${lastName}`;
+  }
+  @Get('/:id')
+  getById(@Param('id') id: string): string {
+    return `hello ${id}`;
+  }
+}
